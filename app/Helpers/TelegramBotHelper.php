8da217b49ddf5749f7fa5b543e161ab5db70cdd5
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2019-04-20 23:49:39 +0700

    Pengusulan tampilan pesan

--- app/Helpers/TelegramBotHelper.php
+++ app/Helpers/TelegramBotHelper.php
@@ -36,8 +36,6 @@ class TelegramBotHelper
     }
 
     public static function detailMessage($message,$total){
-        $notifMessage = "*[KawalPemiluBot2019 Made in MitraAnakNegeri]*"."\n";
-        $notifMessage .= "Hasil: "."\n";
 //        foreach ($message as $item){
 //            $notifMessage .="Nama Provinsi : ".$item['nama_provinsi']."\n";
 //            $notifMessage .="Suara Pasangan 01 : Ir. Joko Widodo dan KH. Ma`ruf Amin : ".$item['pas1']."\n";
@@ -48,16 +46,28 @@ class TelegramBotHelper
 //            $notifMessage .="Total TPS : ".$item['total_tps']."\n";
 //        }
 //
-        $notifMessage .="Total Suara Paslon 01: Ir.Joko Widodo dan KH.Maruf Amin :".number_format($total['total_pas1'],0)."\n";
-        $notifMessage .="Prosentase : ".number_format(($total['total_pas1']/($total['total_sah']+$total['total_tSah']))*100,2)."%"."\n";
-        $notifMessage .="Total Suara Paslon 02: H.Prabowo Subianto dan Sandiaga Uno :".number_format($total['total_pas2'],0)."\n";
-        $notifMessage .="Prosentase : ".number_format(($total['total_pas2']/($total['total_sah']+$total['total_tSah']))*100,2)."%"."\n";
-        $notifMessage .="Total Suara Sah : ".number_format($total['total_sah'],0)."\n";
-        $notifMessage .="Total Suara Tidak Sah :".number_format($total['total_tSah'],0)."\n";
-        $notifMessage .="TPS di Proses : ".$total['tps_proses']."\n";
-        $notifMessage .="Total TPS : ".$total['total_tps']."\n";
-        $notifMessage .="Note : Prosentase lebih dari 100% karena ada data jumlah suara sah dari kedua paslon di beberapa daerah tidak sama dengan total suara sah didaerah tersebut"."\n";
-        $notifMessage .="Data dari kawalpemilu.org";
+        $notifMessage = "*[KawalPemiluBot2019 Made by MitraAnakNegeri]*\n";
+        $notifMessage .= "Hasil : \n\n";
+        $notifMessage .= "--------------------------------------------------\n";
+        $notifMessage .= "*Paslon 1 - Ir.Joko Widodo dan KH.Maruf Amin* \n";
+        $notifMessage .= "--------------------------------------------------\n";
+        $notifMessage .= "Total Suara\t: " .number_format($total['total_pas1'],0)."\n";
+        $notifMessage .= "Prosentase\t: ".number_format(($total['total_pas1']/($total['total_sah']+$total['total_tSah']))*100,2)."%\n\n";
+        $notifMessage .= "--------------------------------------------------\n\n";
+        $notifMessage .= "--------------------------------------------------\n";
+        $notifMessage .= "*Paslon 2 - H.Prabowo Subianto dan Sandiaga Uno* \n";
+        $notifMessage .= "--------------------------------------------------\n";
+        $notifMessage .= "Total Suara\t: ".number_format($total['total_pas2'],0)."\n";
+        $notifMessage .= "Prosentase\t: ".number_format(($total['total_pas2']/($total['total_sah']+$total['total_tSah']))*100,2)."%\n";
+        $notifMessage .= "--------------------------------------------------\n\n";
+        $notifMessage .= "--------------------------------------------------\n";
+        $notifMessage .= "Total Suara Sah\t: ".number_format($total['total_sah'],0)."\n";
+        $notifMessage .= "Total Suara Tidak Sah\t: ".number_format($total['total_tSah'],0)."\n";
+        $notifMessage .= "TPS di Proses\t: ".$total['tps_proses']."\n";
+        $notifMessage .= "Total TPS\t: ".$total['total_tps']."\n";
+        $notifMessage .= "--------------------------------------------------\n";
+        $notifMessage .= "Note\t: Prosentase lebih dari 100% karena ada data jumlah suara sah dari kedua paslon di beberapa daerah tidak sama dengan total suara sah didaerah tersebut"."\n";
+        $notifMessage .= "Sumber data\t: http://kawalpemilu.org";
         //dd($notifMessage);
         try{
             return self::sendMessage($notifMessage);
@@ -65,8 +75,4 @@ class TelegramBotHelper
             die("Gagal mengirim ke telegram");
         }
     }
-
-
-
-
 }
